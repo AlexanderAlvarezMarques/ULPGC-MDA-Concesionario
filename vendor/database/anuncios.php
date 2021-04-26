3496e368af9e305a447b7539c166f78dafb679a7
abrandvelaz <abrandvelaz@gmail.com>
2021-04-26 11:50:05 +0100

    Paso de rama

--- vendor/database/anuncios.php
+++ vendor/database/anuncios.php
@@ -17,16 +17,27 @@ class Anuncios {
 
     //Coger anuncios
     public static function getAdvertisements() {
-        $ads = DB::executeSQL("SELECT * FROM anuncios");
-        
-        if ($ads === null) return null;
-
-        if (count($ads) == 0) return null;
-
-        return $ads;
+        $result = DB::executeSQL("SELECT * FROM anuncios");
+        if (count($result) == 0) {
+          return null;
+        }
+        return $result;
     }
-    
 
+    //Coger un anuncio
+
+    public static function getAdvertisement($idAnuncio) {
+        $result = DB::executeSQL("SELECT * FROM anuncios WHERE idanuncios = ?",[$idAnuncio] );
+        if ($result === null) return null;
+        if (count($result) == 0) return null;
+        return $result;
+
+    }
+
+    //Modificar anuncio
+    public static function updateAdvertisement($nombre, $descripcion, $foto, $precio, $marca, $ano){
+        $result = DB::executeSQL("UPDATE anuncios SET nombre_anuncio, descripcion_anuncio, foto_anuncio, precio_anuncio, marca_anuncio, ano_vehiculo",[$nombre, $descripcion, $foto, $precio, $marca, $ano]);
+        return $result === null ? false : true;
+    }   
 }
-
 ?>
