aa802531c4b382498726d3e68f9c538d8fa09396
alexanderalvarezmarques <alex222en@gmail.com>
2021-04-26 11:01:33 +0000

    update get advertisemente for not advertisments on database

--- vendor/database/anuncios.php
+++ vendor/database/anuncios.php
@@ -18,40 +18,18 @@ class Anuncios {
     //Coger anuncios
     public static function getAdvertisements() {
         $result = DB::executeSQL("SELECT * FROM anuncios");
-        if (count($result) == 0) {
+        if ($result !== null && count($result) == 0) {
           return null;
         }
         return $result;
     }
 
     //Coger un anuncio
-
     public static function getAdvertisement($idAnuncio) {
         $result = DB::executeSQL("SELECT * FROM anuncios WHERE idanuncios = ?",[$idAnuncio] );
         if ($result === null) return null;
         if (count($result) == 0) return null;
         return $result;
-
-    }
-
-    //Coger un anuncio
-    public static function getAdvertisement($idAnuncio) {
-        $ads = DB::executeSQL("SELECT * FROM anuncios WHERE idanuncios = ?",[$idAnuncio] );
-        
-        if ($ads === null) return null;
-
-        if (count($ads) == 0) return null;
-
-        return $ads;
-    }
-    
-    //Update anuncio
-    public static function updateAdvertisement($id,$nombre, $descripcion, $foto, $precio, $marca, $modelo, $ano_vehiculo){
-
-        //$result = DB::executeSQL("UPDATE anuncios SET nombre_anuncio = $nombre, descripcion_anuncio = $descripcion, foto_anuncio = $foto, precio_anuncio = $precio, marca_anuncio = $marca, modelo_anuncio = $modelo, ano_vehiculo = $ano_vehiculo WHERE idanuncios = ?",[$id]);
-        $result = DB::executeSQL("UPDATE anuncios SET nombre_anuncio = ?, descripcion_anuncio = ?, foto_anuncio = ?, precio_anuncio = ?, marca_anuncio = ?, modelo_anuncio = ?, ano_vehiculo_anuncio = ? WHERE idanuncios = ?",[$nombre, $descripcion, $foto, $precio, $marca, $modelo, $ano_vehiculo, $id]);
-
-        return $result === null ? false : true;
 
     }
 
